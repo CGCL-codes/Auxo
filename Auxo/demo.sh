dda3c66190e6fedd5588ff30527fe4c094f524b6
Elvis-Jiang <108129143+Elvis-Jiang@users.noreply.github.com>
2023-03-28 10:20:26 +0800

    Update demo.sh

--- Auxo/demo.sh
+++ Auxo/demo.sh
@@ -1,2 +1,2 @@
 cd bin
-./main -dataset ../../data/lkml/rawdata.txt -result ../result/lkml/txt -fpl 16 -width 100 -cols 4 candiNum 16
+./main -dataset ../../data/lkml/rawdata.txt -result ../result/lkml/txt -fpl 16 -width 100 -cols 4 -candiNum 16
